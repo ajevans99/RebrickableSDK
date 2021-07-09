2d8ff04303d41ee3f1b79f917c90fb23a5062d48
Austin Evans <evansau1@msu.edu>
2021-07-09 19:10:25 -0400

    Convert models to camel case

--- Sources/RebrickableSDK/APIs/Models/LegoColor.swift
+++ Sources/RebrickableSDK/APIs/Models/LegoColor.swift
@@ -3,11 +3,11 @@ import Foundation
 public struct LegoColor: Codable, Hashable {
     public let id: Int
     public let name: String?
-    public let is_trans: Bool
-    
-    public init(id: Int, name: String?, is_trans: Bool) {
+    public let isTrans: Bool
+
+    public init(id: Int, name: String?, isTrans: Bool) {
         self.id = id
         self.name = name
-        self.is_trans = is_trans
+        self.isTrans = isTrans
     }
 }
--- Sources/RebrickableSDK/APIs/Models/LegoPartCategory.swift
+++ Sources/RebrickableSDK/APIs/Models/LegoPartCategory.swift
@@ -3,11 +3,11 @@ import Foundation
 public struct LegoPartCategory: Codable, Hashable {
     public let id: Int
     public let name: String
-    public let part_count: Int
-    
-    public init(id: Int, name: String, part_count: Int) {
+    public let partCount: Int
+
+    public init(id: Int, name: String, partCount: Int) {
         self.id = id
         self.name = name
-        self.part_count = part_count
+        self.partCount = partCount
     }
 }
